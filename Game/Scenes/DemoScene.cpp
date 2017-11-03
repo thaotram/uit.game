1844f6b1b9d46540f7fca5b94484b6d2652fe2e3
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2017-11-03 19:52:31 +0700

    Update DemoScene

--- Game/Scenes/DemoScene.cpp
+++ Game/Scenes/DemoScene.cpp
@@ -1,11 +1,5 @@
 ﻿#include "DemoScene.h"
 
-#include "../GameComponents/Unit/Unit.h"
-#include "../GameComponents/Sprite/Sprite.h"
-
-//Unit * mUnits;
-Unit * mAladdin;
-
 DemoScene::DemoScene()
 {
 	LoadContent();
@@ -13,45 +7,17 @@ DemoScene::DemoScene()
 
 void DemoScene::LoadContent()
 {
-	mBackColor = 0xdddddd;
-
-	mAladdin = new Unit("Aladdin", 0xffff00ff);
-	mAladdin->GetSprite()->SetScale(3, 3);
-	mAladdin->GetSprite()->SetPosition(220, 220);
-}
-
-void DemoScene::Update(float dt)
-{
-	mAladdin->Update(dt);
-}
-
-void DemoScene::Draw()
-{
-	mAladdin->Draw();
+	// NULL
 }
 
 void DemoScene::OnKeyDown(int keyCode)
 {
-	if (keyCode == VK_LEFT)
-	{
-		mAladdin->GetSprite()->SetPositionTo(-10, 0);
-	}
-
-	if (keyCode == VK_RIGHT)
-	{
-		mAladdin->GetSprite()->SetPositionTo(10, 0);
-	}
-
-	if (keyCode == VK_UP) {
-		mAladdin->SetFrame(1);
-	}
+	if (keyCode == VK_LEFT) {}
+	if (keyCode == VK_RIGHT) {}
+	if (keyCode == VK_UP) {}
 	if (keyCode == VK_DOWN) {}
 }
 
-void DemoScene::OnKeyUp(int keyCode)
-{
-}
+void DemoScene::OnKeyUp(int keyCode) {}
 
-void DemoScene::OnMouseDown(float x, float y)
-{
-}
+void DemoScene::OnMouseDown(float x, float y) {}
--- Game/Scenes/DemoScene.h
+++ Game/Scenes/DemoScene.h
@@ -5,25 +5,15 @@
 #include <d3d9.h>
 
 #include "../GameComponents/Scene/Scene.h"
+#include "../GameComponents/Node/Unit.h"
 
 using namespace std;
-//#include "../GameComponents/Sprite.h"
-//#include "../GameComponents/Animation.h"
 
 class DemoScene : public Scene
 {
 public:
-	DemoScene();
-
-	void Update(float dt);
-	void LoadContent();
-	void Draw();
-
-	void OnKeyDown(int keyCode);
-	void OnKeyUp(int keyCode);
-	void OnMouseDown(float x, float y);
-protected:
-	//Animation *mGoldBlock;
-	float mTimeCounter;
+	DemoScene() {
+		mNode->SetSprite(new Unit("Aladdin", 0x00));
+	}
 };
 
