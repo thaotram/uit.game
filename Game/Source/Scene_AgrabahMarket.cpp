cb9a1de69ff4cf98271ed6bad167b98bef59e75a
thaotram <sawa.kisei@gmail.com>
2017-11-18 23:23:15 +0700

    civilian,enemyexplosion full

--- Game/Source/Scene_AgrabahMarket.cpp
+++ Game/Source/Scene_AgrabahMarket.cpp
@@ -1,12 +1,14 @@
 #include "Scene_AgrabahMarket.h"
 #include "Unit_Aladdin.h"
 #include "Unit_Peddler.h"
+#include "Unit_EnemyExplosions.h"
+#include "Unit_CivilianEnemies.h"
 
 AgrabahMarket::AgrabahMarket() {
-	(*this)["2"] = new Unit_Peddler();
+	(*this)["2"] = new Unit_CivilianEnemies();
 	(*this)["2"]->SetPosition(
 		GameGlobal::GetWidth() / 2 * 1.f, 
 		GameGlobal::GetScale() * 189
 	);
-	(*this)["2"]->GetAnimation()->SetState("peddler_show");
+	(*this)["2"]->GetAnimation()->SetState("straw_hit");
 }
---
+++ Game/Source/Unit_EnemyExplosions.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Unit.h"
+class Unit_EnemyExplosions : public Unit {
+private:
+
+public:
+	Unit_EnemyExplosions() : Unit("EnemyExplosions") {
+
+	}
+};
