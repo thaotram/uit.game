8a2e3b95067ffb89a2c95f603f60d588f804c251
thaotram <sawa.kisei@gmail.com>
2017-11-19 09:39:07 +0700

    Abu

--- Game/Source/Scene_AgrabahMarket.cpp
+++ Game/Source/Scene_AgrabahMarket.cpp
@@ -3,12 +3,13 @@
 #include "Unit_Peddler.h"
 #include "Unit_EnemyExplosions.h"
 #include "Unit_CivilianEnemies.h"
+#include "Unit_Abu.h"
 
 AgrabahMarket::AgrabahMarket() {
-	(*this)["2"] = new Unit_CivilianEnemies();
+	(*this)["2"] = new Unit_Abu();
 	(*this)["2"]->SetPosition(
 		GameGlobal::GetWidth() / 2 * 1.f, 
 		GameGlobal::GetScale() * 189
 	);
-	(*this)["2"]->GetAnimation()->SetState("straw_hit");
+	(*this)["2"]->GetAnimation()->SetState("abu_levelcomplete");
 }
