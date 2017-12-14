69778b810511b2d587dda9bf16631e5ee64f213f
SofiaJetson02081997 <henrynguyen02081997@gmail.com>
2017-12-14 19:27:16 +0700

    Huyền up

--- Game/Source/Object_Status_Final/Object_Status_Number_S.cpp
+++ Game/Source/Object_Status_Final/Object_Status_Number_S.cpp
@@ -1,12 +1,9 @@
 #include "Object_Status_Number_S.h"
+#define mAni	mAnimation
+#define mPos	mPosition
+#define speedX	490
 
-
-
-Object_Status_Number_S::Object_Status_Number_S()
-{
-}
-
-
-Object_Status_Number_S::~Object_Status_Number_S()
-{
+Object_Status_Number_S::Object_Status_Number_S() : Object_Status("Status") {
+	mPos << V2{ 200  , 600 };
+	mAni.Set("number_s", 1);
 }
