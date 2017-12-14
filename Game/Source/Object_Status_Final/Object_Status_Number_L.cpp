e2fa9dd349f0f29b7845eed9058fd614700aa7ad
SofiaJetson02081997 <henrynguyen02081997@gmail.com>
2017-12-14 19:31:51 +0700

    Edit Status

--- Game/Source/Object_Status_Final/Object_Status_Number_L.cpp
+++ Game/Source/Object_Status_Final/Object_Status_Number_L.cpp
@@ -1,5 +1,9 @@
 #include "Object_Status_Number_L.h"
+#define mAni	mAnimation
+#define mPos	mPosition
+#define speedX	490
 
-Object_Status_Number_L::Object_Status_Number_L() : Object_Status("Status")
-{
+Object_Status_Number_L::Object_Status_Number_L() : Object_Status("Status") {
+	mPos << V2{ 200  , 600 };
+	mAni.Set("number_l", 1);
 }
