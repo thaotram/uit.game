4593182b03f6705f6df36f17edf361e3237aa946
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2017-12-14 19:26:02 +0700

    Lên

--- Game/Source/Object_Status_Final/Object_Status_Number_S.cpp
+++ Game/Source/Object_Status_Final/Object_Status_Number_S.cpp
@@ -1,12 +1,5 @@
 #include "Object_Status_Number_S.h"
 
-
-
-Object_Status_Number_S::Object_Status_Number_S()
-{
-}
-
-
-Object_Status_Number_S::~Object_Status_Number_S()
+Object_Status_Number_S::Object_Status_Number_S() : Object_Status("Status")
 {
 }
