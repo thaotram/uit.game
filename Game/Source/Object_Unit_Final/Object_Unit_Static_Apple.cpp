6c4365c7e0a106dc51968c517fffc47d850e0fac
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2017-12-15 17:28:50 +0700

    Static Apple

--- Game/Source/Object_Unit_Final/Object_Unit_Static_Apple.cpp
+++ Game/Source/Object_Unit_Final/Object_Unit_Static_Apple.cpp
@@ -5,16 +5,12 @@ Object_Unit_Static_Apple::Object_Unit_Static_Apple(float x, float y) : Object_Un
 	mPos << V2{ x , y };
 	mAni.Set("apple_item", 1);
 	mAutoNextFrame = false;
-	mIs = false;
 }
 void Object_Unit_Static_Apple::ObjectUpdateEvent(float dt) {
 	
 }
 
 void Object_Unit_Static_Apple::ObjectIntersect(){
-	mAni.SetCycleIndex(2);
-	if (!mIs) {
-		mIs = true;
-		mScene->mApple++;
-	}
+	mScene->mApple++;
+	mScene->AddToRemoveList(this);
 }
--- Game/Source/Object_Unit_Final/Object_Unit_Static_Apple.h
+++ Game/Source/Object_Unit_Final/Object_Unit_Static_Apple.h
@@ -3,8 +3,6 @@
 #include "../Scene/Scene.h"
 
 class Object_Unit_Static_Apple final : public Object_Unit {
-private:
-	bool mIs;
 public:
 	Object_Unit_Static_Apple(float x, float y);
 	~Object_Unit_Static_Apple() {};
