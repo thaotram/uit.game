e02b64ede533db72695d62a24f2f114665364d42
Unknown <sawa.kisei@gmail.com>
2017-12-16 20:16:19 +0700

    Auto stash before merge of "master" and "origin/master"

---
+++ Game/Source/Object_Unit_Final/Object_Unit_Knife.cpp
@@ -0,0 +1,26 @@
+﻿#include "Object_Unit_Knife.h"
+#define mAni	mAnimation
+#define mPos	mPosition
+#define	mBlock	mScene->mMapBlock
+#define tSpeedX	400
+//#define tSpeedX	490
+
+#define xx mPos.x()
+#define yy mPos.y()
+
+//# Ném dao của Fat & Circus
+
+Object_Unit_Knife::Object_Unit_Knife(float x, float y) : Object_Unit("Guards"){
+}
+
+Object_Unit_Knife::~Object_Unit_Knife()
+{
+}
+
+void Object_Unit_Knife::ObjectUpdateEvent(float dt)
+{
+}
+
+void Object_Unit_Knife::ThrowApple(bool flip)
+{
+}
---
+++ Game/Source/Object_Unit_Final/Object_Unit_Knife.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "../Object_Unit/Object_Unit.h"
+#include "../GameDebug.h"
+
+class Object_Unit_Knife final : public Object_Unit{
+private:
+	RECT tDis;
+	RECT tUnit;
+	bool mFlip;
+public:
+	Object_Unit_Knife(float x, float y);
+	~Object_Unit_Knife();
+
+	bool mIsThrow;
+	void ObjectUpdateEvent(float dt);
+	void ThrowApple(bool flip);
+
+};
+
