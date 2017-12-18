eeda340f7ec8e3959ff4570b46cde0272d1fdf1d
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2017-12-18 09:25:15 +0700

    Style

--- Game/Source/Object_Unit_Final/Object_Unit_NPC_Camel.cpp
+++ Game/Source/Object_Unit_Final/Object_Unit_NPC_Camel.cpp
@@ -1,21 +1,17 @@
-	#include "Object_Unit_NPC_Camel.h"
-#define mAni			mAnimation
-#define mPos			mPosition
-#define	mObjectStore	mScene->oObjectStore
+#include "Object_Unit_NPC_Camel.h"
+#define mAni mAnimation
+#define mPos mPosition
+#define mObjectStore mScene->oObjectStore
 
 #define xx mPos.x()
 #define yy mPos.y()
 
 Object_Unit_NPC_Camel::Object_Unit_NPC_Camel(RECT u) : Object_Unit("Camel") {
-	mPos.x << (float)(u.left);
-	mPos.y << (float)(u.top);
-	mAni.Set("pedal", 1);
-}
-
-Object_Unit_NPC_Camel::~Object_Unit_NPC_Camel() {
-
+    mPos.x << (float)(u.left);
+    mPos.y << (float)(u.top);
+    mAni.Set("pedal", 1);
 }
 
 void Object_Unit_NPC_Camel::StartAnimation() {
-	mAni.SetCycleIndex(1);
+    mAni.SetCycleIndex(1);
 }
--- Game/Source/Object_Unit_Final/Object_Unit_NPC_Camel.h
+++ Game/Source/Object_Unit_Final/Object_Unit_NPC_Camel.h
@@ -1,12 +1,11 @@
 #pragma once
-#include "../Object_Unit/Object_Unit.h"
 #include "../GameDebug.h"
+#include "../Object_Unit/Object_Unit.h"
 
-class Object_Unit_NPC_Camel final : public Object_Unit
-{
-public:
-	Object_Unit_NPC_Camel(RECT u);
-	~Object_Unit_NPC_Camel();
+class Object_Unit_NPC_Camel final : public Object_Unit {
+   public:
+    Object_Unit_NPC_Camel(RECT u);
+    ~Object_Unit_NPC_Camel(){};
 
-	void StartAnimation();
+    void StartAnimation();
 };
