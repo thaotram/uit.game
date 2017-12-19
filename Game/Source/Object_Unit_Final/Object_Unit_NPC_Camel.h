82efc7f2abf87b806e5cb4dbc8ecdcc2d86e5968
SofiaJetson02081997 <henrynguyen02081997@gmail.com>
2017-12-19 17:47:15 +0700

    huyen len

--- Game/Source/Object_Unit_Final/Object_Unit_NPC_Camel.h
+++ Game/Source/Object_Unit_Final/Object_Unit_NPC_Camel.h
@@ -2,10 +2,14 @@
 #include "../GameDebug.h"
 #include "../Object_Unit/Object_Unit.h"
 
+
 class Object_Unit_NPC_Camel final : public Object_Unit {
-   public:
+private:
+	bool mIsSpitOut;
+public:
     Object_Unit_NPC_Camel(RECT u);
     ~Object_Unit_NPC_Camel(){};
 
     void StartAnimation();
+	void ObjectUpdateEvent(float dt);
 };
--- Game/Source/Object_Unit_Final/Object_Unit_Savila.cpp
+++ Game/Source/Object_Unit_Final/Object_Unit_Savila.cpp
@@ -1,4 +1,5 @@
 #include "Object_Unit_Savila.h"
+#include "Object_Unit_NPC_Camel.h"
 #define mAni	mAnimation
 #define mPos	mPosition
 #define	mObjectStore	mScene->oObjectStore
@@ -6,10 +7,13 @@
 #define xx mPos.x()
 #define yy mPos.y()
 
-Object_Unit_Savila::Object_Unit_Savila(float x, float y): Object_Unit("")
+Object_Unit_Savila::Object_Unit_Savila(float x, float y) : Object_Unit("Camel")
 {
+	mPos << V2{ x,y };
+	mAni.Set("savila", 1);
+	mTimePerFrame = 0.03f;
+	mAutoNextFrame = false;
 }
-
 
 Object_Unit_Savila::~Object_Unit_Savila()
 {
@@ -17,4 +21,21 @@ Object_Unit_Savila::~Object_Unit_Savila()
 
 void Object_Unit_Savila::ObjectUpdateEvent(float dt)
 {
+	tUnit = RECT{
+		(LONG)xx,
+		(LONG)yy,
+		(LONG)xx,
+		(LONG)yy
+	};
+	tDis = mObjectStore->GetDistance(tUnit, this);
+
+	mPos.x += tSpeedX*dt;
+	mPos.x.Update(dt);
+	//if (tDis.right <= (xCamel + 42 + 20)) {
+	//}
+	//else {
+	//	if (mAni.GetCycleIndex() == 8) {
+	//		mIsMarkedDelete = true;
+	//	}
+	//}
 }
