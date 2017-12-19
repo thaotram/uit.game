1fe42d28a82aeab46e93f318eab12b82d14098d3
thaotram <sawa.kisei@gmail.com>
2017-12-19 22:59:39 +0700

    boss nè

--- Game/Source/Object_Unit_Final/Object_Unit_Enemy_Thin.cpp
+++ Game/Source/Object_Unit_Final/Object_Unit_Enemy_Thin.cpp
@@ -2,9 +2,12 @@
 #define mAni			mAnimation
 #define mPos			mPosition
 #define	mObjectStore	mScene->oObjectStore
+#define isFlip			(mTransform.GetFlip())
 
 #define xx mPos.x()
 #define yy mPos.y()
+
+#define state	mAni.GetState()
 
 Object_Unit_Enemy_Thin::Object_Unit_Enemy_Thin(RECT u) : Object_Unit("Guards") {
 	mPos.x << (float)(u.left + u.right) / 2;
@@ -18,5 +21,38 @@ Object_Unit_Enemy_Thin::~Object_Unit_Enemy_Thin() {}
 void Object_Unit_Enemy_Thin::ObjectUpdateEvent(float dt){
 	if (mHealthPoint == 0) {
 		mIsMarkedDelete = true;
+	}
+}
+
+void Object_Unit_Enemy_Thin::ObjectEachState()
+{
+	if (state == "") {
+		mAni.Set("thin_stand", 1);
+	}
+	else if (state == " thin_run") {
+		mAutoNextFrame = true;
+	}
+	else if (state == "thin_hit") {
+		if (mAni.GetCycleIndex() == 3) {
+			tUnitDamage = RECT{
+				(LONG)((isFlip) ? (xx - 63) : (xx + 16)),
+				(LONG)(yy - 68),
+				(LONG)((isFlip) ? (xx - 16) : (xx + 63)),
+				(LONG)(yy - 6)
+			}
+		if (mAni.GetCycleIndex() == 4) {
+			tUnitDamage = RECT{
+				(LONG)((isFlip) ? (xx - 63) : (xx + 16)),
+				(LONG)(yy - 68),
+				(LONG)((isFlip) ? (xx - 16) : (xx + 63)),
+				(LONG)(yy - 6)
+			}
+		if (mAni.GetCycleIndex() == 5) {
+			tUnitDamage = RECT{
+				(LONG)((isFlip) ? (xx - 63) : (xx + 16)),
+				(LONG)(yy - 68),
+				(LONG)((isFlip) ? (xx - 16) : (xx + 63)),
+				(LONG)(yy - 6)
+			}
 	}
 }
--- Game/Source/Object_Unit_Final/Object_Unit_Enemy_Thin.h
+++ Game/Source/Object_Unit_Final/Object_Unit_Enemy_Thin.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "../Object_Unit/Object_Unit.h"
 #include "../GameDebug.h"
+#include "../Scene/Scene.h"
 
 class Object_Unit_Enemy_Thin final : public Object_Unit
 {
@@ -8,5 +9,6 @@ public:
 	Object_Unit_Enemy_Thin(RECT u);
 	~Object_Unit_Enemy_Thin();
 	void ObjectUpdateEvent(float dt);
+	void ObjectEachState();
 };
 
