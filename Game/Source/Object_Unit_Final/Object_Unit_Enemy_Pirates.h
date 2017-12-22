ba29247a1e92993b2da768e4344e017ff6ad86de
thaotram <sawa.kisei@gmail.com>
2017-12-22 14:52:53 +0700

    đô con

--- Game/Source/Object_Unit_Final/Object_Unit_Enemy_Pirates.h
+++ Game/Source/Object_Unit_Final/Object_Unit_Enemy_Pirates.h
@@ -4,8 +4,9 @@
 #include "../GameDebug.h"
 #include "../Scene/Scene.h"
 
-class Object_Unit_Enemy_Pirates final : public Object_Unit
-{
+class Object_Unit_Enemy_Pirates final : public Object_Unit{
+private:
+	RECT mLimit;
 public:
 	Object_Unit_Enemy_Pirates(RECT u);
 	~Object_Unit_Enemy_Pirates();
