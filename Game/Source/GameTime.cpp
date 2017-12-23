0ca7ed97cf9ac1d822a6444274c4c9a4439d93ea
phamhongphuc <phamhongphuc.info@gmail.com>
2017-12-23 20:04:05 +0700

    Fix Lag

--- Game/Source/GameTime.cpp
+++ Game/Source/GameTime.cpp
@@ -4,14 +4,12 @@ LARGE_INTEGER GameTime::mStartTime;
 LARGE_INTEGER GameTime::mEndTime;
 LARGE_INTEGER GameTime::mFrequency;
 float count = 0;
-void GameTime::StartCounter()
-{
+void GameTime::StartCounter() {
 	if (!QueryPerformanceFrequency(&mFrequency)) return;
 	QueryPerformanceCounter(&mStartTime);
 }
 
-float GameTime::GetCounter()
-{
+float GameTime::GetCounter() {
 	QueryPerformanceCounter(&mEndTime);
 	return static_cast<float>((mEndTime.QuadPart - mStartTime.QuadPart)) / static_cast<float>(mFrequency.QuadPart);
 }
