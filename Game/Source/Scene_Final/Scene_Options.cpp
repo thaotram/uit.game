61d66b1fc309a0b2ea934dea816b18a3a38f99b1
SofiaJetson02081997 <henrynguyen02081997@gmail.com>
2017-12-26 17:44:03 +0700

    Dang lam

--- Game/Source/Scene_Final/Scene_Options.cpp
+++ Game/Source/Scene_Final/Scene_Options.cpp
@@ -1,12 +1,18 @@
 #include "Scene_Options.h"
 
+#include "../Object_Screen/Object_Screen.h"
+#include "../../Define.h"
 
-
-Scene_Options::Scene_Options()
+Scene_Options::Scene_Options() : Scene()
 {
+	oObjectStore = new Scene_ObjectStore("");
+	mTimer = 0;
 }
-
 
 Scene_Options::~Scene_Options()
 {
 }
+
+//void Scene_Options::SceneRender(float delay)
+//{
+//}
--- Game/Source/Scene_Final/Scene_Options.h
+++ Game/Source/Scene_Final/Scene_Options.h
@@ -1,9 +1,16 @@
 #pragma once
+#include "../Scene/Scene.h"
 
-class Scene_Options
+using namespace std;
+
+class Scene_Options final:public Scene
 {
+private:
+	Scene* pLastScene;
+	float mTimer;
 public:
 	Scene_Options();
 	~Scene_Options();
+	//void SceneRender(float delay);
 };
 
