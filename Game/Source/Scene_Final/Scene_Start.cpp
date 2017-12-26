d6bfe49d593ad8cbd16ca3d91e756854424feaaf
SofiaJetson02081997 <henrynguyen02081997@gmail.com>
2017-12-26 20:05:55 +0700

    Scene_Start (cont.)

--- Game/Source/Scene_Final/Scene_Start.cpp
+++ Game/Source/Scene_Final/Scene_Start.cpp
@@ -1,12 +1,21 @@
 #include "Scene_Start.h"
 
+#include "../../Define.h"
+#include "../Object_Map_Final/Object_Map_Scene_Start.h"
+#include "../Object_Screen/Object_Screen.h"
+#include "../Scene_Final/Scene_AgrabahMarket.h"
 
+#define I GameGlobal::Input
+#define E I[ENTER]
 
-Scene_Start::Scene_Start()
+Scene_Start::Scene_Start() : Scene()
 {
+	oObjectStore = new Scene_ObjectStore("");
+	oMapBackground = new Object_Screen("Option_Scene", "main_menu");
 }
 
-
-Scene_Start::~Scene_Start()
+void Scene_Start::SceneRender(float delay)
 {
+	oMapBackground->ObjectUpdateEvent(delay);
+	oMapBackground->ObjectRender(delay);
 }
--- Game/Source/Scene_Final/Scene_Start.h
+++ Game/Source/Scene_Final/Scene_Start.h
@@ -1,8 +1,21 @@
 #pragma once
-class Scene_Start
+#include "../GameDebug.h"
+#include "../Scene/Scene.h"
+#include "../Scene/Scene_ObjectStore.h"
+#include "../../Define.h"
+
+using namespace std;
+
+class Scene_Start final :public Scene
 {
+private:
+	Object* oMapBackground;
+	//Scene* pLastScene;
+	//float mTimer;
 public:
 	Scene_Start();
-	~Scene_Start();
+	~Scene_Start() {};
+	void SceneRender(float delay);
+	//void ObjectIntersect(Object * pObject);
 };
 
