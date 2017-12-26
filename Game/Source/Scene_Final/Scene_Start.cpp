17000251e322e722e8804756a2f9de9ba3b08ced
SofiaJetson02081997 <henrynguyen02081997@gmail.com>
2017-12-26 20:25:31 +0700

    Scene_Start (Finish)

--- Game/Source/Scene_Final/Scene_Start.cpp
+++ Game/Source/Scene_Final/Scene_Start.cpp
@@ -1,21 +1,26 @@
 #include "Scene_Start.h"
 
 #include "../../Define.h"
-#include "../Object_Map_Final/Object_Map_Scene_Start.h"
 #include "../Object_Screen/Object_Screen.h"
 #include "../Scene_Final/Scene_AgrabahMarket.h"
 
 #define I GameGlobal::Input
-#define E I[ENTER]
+#define Z I[CHAR_Z]
+#define X I[CHAR_X]
+#define C I[CHAR_C]
 
 Scene_Start::Scene_Start() : Scene()
 {
+
 	oObjectStore = new Scene_ObjectStore("");
 	oMapBackground = new Object_Screen("Option_Scene", "main_menu");
 }
 
 void Scene_Start::SceneRender(float delay)
 {
+	if (Z || X || C){
+		Scene::NextScene(new Scene_AgrabahMarket());
+	}
 	oMapBackground->ObjectUpdateEvent(delay);
 	oMapBackground->ObjectRender(delay);
 }
