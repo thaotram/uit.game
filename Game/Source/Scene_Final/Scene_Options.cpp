3e9c53e86dc7ef6958e20e50b0dde259d91c7a42
SofiaJetson02081997 <henrynguyen02081997@gmail.com>
2017-12-26 19:17:43 +0700

    up

--- Game/Source/Scene_Final/Scene_Options.cpp
+++ Game/Source/Scene_Final/Scene_Options.cpp
@@ -6,11 +6,15 @@
 Scene_Options::Scene_Options() : Scene()
 {
 	oObjectStore = new Scene_ObjectStore("");
+	oStatus = new Scene_Status();
+	oMapBackground = new Object_Screen("Screen", "JafarPalace_Background");
+
 	mTimer = 0;
 }
 
 Scene_Options::~Scene_Options()
 {
+
 }
 
 //void Scene_Options::SceneRender(float delay)
--- Game/Source/Scene_Final/Scene_Options.h
+++ Game/Source/Scene_Final/Scene_Options.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "../Scene/Scene.h"
+#include "../Scene/Scene_Status.h"
+#include "../Scene/Scene_ObjectStore.h"
 
 using namespace std;
 
@@ -8,9 +10,11 @@ class Scene_Options final:public Scene
 private:
 	Scene* pLastScene;
 	float mTimer;
+	Object* oMapBackground;
+	Scene_Status* oStatus;
 public:
 	Scene_Options();
 	~Scene_Options();
-	//void SceneRender(float delay);
+	void SceneRender(float delay);
 };
 
